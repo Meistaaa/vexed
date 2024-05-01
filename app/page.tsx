fffed58b89b3be0df4534efd30e51c6b60f60fb8
Meistaaa <alimuhammad9868@gmail.com>
2024-05-02 03:55:47 +0500

    video section background

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 import Background from "@/components/Background";
 import HeroSection from "@/components/HeroSection";
 import Services from "@/components/Services";
-import Testimonials from "@/components/Testimonials";
-import TestimonialsImage from "@/components/TestimonialsImage";
-import VideoSection from "@/components/VideoSection";
+import VideoSectionBackground from "@/components/VideoSectionBackground";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <div></div>
       <div className="background-gradient h-fit ">
         <HeroSection></HeroSection>
       </div>
-      <div className="bg-bgBlue h-[983px] overflow-hidden flex items-center justify-center">
-        <VideoSection></VideoSection>
+      <div className="bg-bgBlue flex flex-col items-center justify-end  overflow-hidden">
+        <VideoSectionBackground></VideoSectionBackground>
       </div>
       <div className="h-[983px] new-gradient flex items-center justify-center">
         <Services></Services>
--- components/VideoSectionBackground.tsx
+++ components/VideoSectionBackground.tsx
@@ -1,5 +1,47 @@
+import Image from "next/image";
+
 const VideoSectionBackground = () => {
-  return <div>VideoSectionBackground</div>;
+  return (
+    <div className="flex flex-col items-start h-full">
+      <div className="self-center pt-24">
+        <h2 className="font-semibold text-4xl text-center text-white mb-4">
+          Generate ad creatives that help you sell. Fast.
+        </h2>
+        <h1 className="text-center text-red text-6xl font-bold mb-2">
+          Using Artificial Intelligence
+        </h1>
+        <p className="text-lightGray mt-4 text-lg text-center mb-24">
+          In 2023, every single thing in Digital Marketing is optimized by
+          Artificial Intelligence, except Creatives. Vexed solution{" "}
+        </p>
+      </div>
+      <div className="mt-auto">
+        <div className="relative   w-0 h-0 border border-b-[#F5F5FF] border-x-[transparent] border-t-transparent border-x-[700px] border-b-[600px] ">
+          <div className="absolute top-[400px]  left-[50%] transform -translate-x-1/2 rotate-180 w-fit"></div>
+          <div className="z-0 absolute right-[467px] top-[120px] rotate-[49.5deg]  w-0 h-0 border border-b-[#332b5480] border-x-[transparent] border-t-transparent border-x-[25px] border-b-[600px] "></div>
+          <div className="z-0 absolute left-[467px] top-[120px] -rotate-[49.5deg]  w-0 h-0 border border-b-[#332b5480] border-x-[transparent] border-t-transparent border-x-[25px] border-b-[600px] "></div>
+          <div className="flex items-center justify-center z-30 ">
+            <div className="p-4  absolute -top-[50px] flex items-center justify-center w-[1134px] h-[567px] video-background-gradient rounded-[20px]">
+              <div className="border-8 flex items-center justify-center border-white  bg-[#D9D9D9] h-[100%] rounded-[20px] w-[100%]">
+                <button
+                  type="button"
+                  className="w-24 h-24 relative bg-white flex items-center justify-center rounded-full z-40 "
+                >
+                  <Image
+                    src={"/polygon3.png"}
+                    alt="polygon3"
+                    height={48}
+                    width={48}
+                    className="ml-3"
+                  ></Image>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoSectionBackground;
